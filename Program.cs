a054f70fcd5bf232fdbad226b307fc789e5e8b4f
TaylorSwiftQueen <92420980+TaylorSwiftQueen@users.noreply.github.com>
2021-10-13 02:17:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFyiT3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOrganizationService service = ConnectionFactory.GetCrmService();
+            Console.WriteLine("Qual oportunidade você deseja aplicar o desconto ?");
+            string Id = Console.ReadLine();
+            
+            Update update = new Update(service);
+            
+            EntityCollection parentsid = update.RetrieveParentId(new Guid(Id),Id);
+
+            foreach (Entity parentid in parentsid.Entities)
+            {
+                string NivelDoCliente = parentid["g07_niveldocliente"].ToString();
+                Console.WriteLine(parentid["g07_niveldocliente"].ToString());
+                EntityCollection nivels = update.RetrieveNivelDoCliente(new Guid("e2e013c9-cd27-ec11-b6e6-002248372ef6"),NivelDoCliente);
+               
+                foreach (Entity nivel in nivels.Entities)
+                {
+
+
+                    Console.WriteLine(nivel["g07_valordedesconto"].ToString());
+
+                    Console.WriteLine("Voce deseja atualizar essa oportunidade?");
+
+                    char S = char.Parse(Console.ReadLine());
+
+                    if (S == 's')
+                    {
+                        Console.WriteLine("Qual o novo valor de desconto?");
+                        string desc = Console.ReadLine();
+                        update.UpdateDesc(desc);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Obrigado!!");
+                    }
+                }
+            }
+
+            
+            
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Update.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace ProjetoFyiT3
+{
+    public class Update
+    {
+        
+
+        public IOrganizationService Service { get; set; }
+
+        public Update(IOrganizationService service)
+        {
+            this.Service = service;
+        }
+
+
+        public EntityCollection RetrieveParentId(Guid parentId,string id)
+        {
+            QueryExpression queryOpportunity = new QueryExpression("opportunity");
+            queryOpportunity.ColumnSet.AddColumns("g07_niveldocliente");
+            queryOpportunity.Criteria.AddCondition("opportunityid", ConditionOperator.Equal, id);
+            
+            return this.Service.RetrieveMultiple(queryOpportunity);
+        }
+
+        public EntityCollection RetrieveNivelDoCliente(Guid parentId,string desc)
+        {
+            QueryExpression queryNivelDoCliente = new QueryExpression("g07_niveldocliente");
+            queryNivelDoCliente.ColumnSet.AddColumns("g07_valordedesconto");
+            queryNivelDoCliente.Criteria.AddCondition("g07_niveldocliente", ConditionOperator.Equal, desc);
+
+            return this.Service.RetrieveMultiple(queryNivelDoCliente);
+        }
+
+        public void UpdateDesc(string desc)
+        {
+            Entity nivel = new Entity("g07_niveldocliente");
+            nivel.Id = new Guid("e2e013c9-cd27-ec11-b6e6-002248372ef6");
+            nivel["g07_discountamount"] = desc;
+            this.Service.Update(nivel);
+        }
+    }
+}
